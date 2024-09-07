431a53bcdb73a4b3230b41a1eb8c38346d5c521b
Mxttosierrv <mat.abarca@duocuc.cl>
2024-09-07 16:42:57 -0400

    Implementación carrusel de imagenes en INICIO

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { CarritoService, Product } from '../../services/carrito.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,16 @@ import { CarritoService, Product } from '../../services/carrito.service';
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class InicioPage implements OnInit {
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+    slidesPerView: 1, // Mostrar 1 slide a la vez
+    autoplay: true, // Hacer que las imágenes se cambien automáticamente
+  };
+
   products: Product[] = [];
 
   constructor(private cartService: CarritoService) {}
@@ -18,7 +27,7 @@ export class InicioPage implements OnInit {
   ngOnInit() {
     this.products = [
       { id: 1, nombre: 'Completo Italiano', descripcion: 'Palta, tomate y mayo', precio: 3000, cantidad: 0 },
-      { id: 2, nombre: 'Completo Palta', descripcion: 'Palta', precio: 3500, cantidad: 0 },
+      { id: 2, nombre: 'Completo Palta', descripcion: 'Palta', precio: 3500, cantidad: 0},
     ];
   }
 
